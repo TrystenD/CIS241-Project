ff519708202f0099705539dcf2c3302a20eed9d3
dembeckt <dembeckt@mail.gvsu.edu>
2022-06-14 17:30:29 -0400

    Initial commit of main.c file

---
+++ Code/SPYProject/main.c
@@ -0,0 +1,31 @@
+/**
+ * @brief This program parses data from a data file
+ *        containing SPY put call information and performs
+ *        statistical calculations to make inferences
+ *        on the state of the market.
+ *
+ * @author Trysten Dembeck
+ *
+ * @file main.c
+ *
+ * @note Assignmnet: SPY Project
+ *       Course:     CIS241-01
+ *       Instructor: Prof. Bhuse
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
+/**
+ * @brief
+ *
+ * @param
+ * @return
+ */
+
